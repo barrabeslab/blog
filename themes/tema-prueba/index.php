0a3f63fb83ec45eb7c4684f7026ae77d76da17a3
Naiara <naiara.castellanos@barrabesmeaning.com>
2018-01-23 14:10:33 +0100

    new styles without plugin

--- themes/tema-prueba/index.php
+++ themes/tema-prueba/index.php
@@ -8,7 +8,7 @@
 						$args = array( 'numberposts' => 1 );
 						$lastposts = get_posts( $args );
 						foreach($lastposts as $post) : setup_postdata($post); ?>
-							<article style="background:url(
+							<article class="post-article" style="background:url(
 								<?php
 								if ( has_post_thumbnail() ) {
 									the_post_thumbnail_url('full');
@@ -63,58 +63,67 @@
 
 		<section class="top-section">
 			<div class="container">
-				<h2 class="title-2 color-underline">Top Stories
-					<!-- <span class="light-text">for you</span> -->
-				</h2>
-				<?php dynamic_sidebar( 'sidebar-4' ); ?>
-				<!-- <div class="articles">
-					<article class="top-article">
-						<figure>
-							<img src="<?php bloginfo('template_url'); ?>/img/hero.png" alt="">
-						</figure>
-						<h3 class="color-title-cat1"> Technology</h3>
-						<h4 class="basic-font">
-							Here's The Simple Trick To Look Your Best In Selfies From Your Smartphone
-
-						</h4>
+				<h2 class="title-2 color-underline">Top Stories</h2>
+				<div class="popular-posts">
+					<?php $pc = new WP_Query('orderby=comment_count&posts_per_page=10');
+					while ($pc->have_posts()) : $pc->the_post(); ?>
+						<article class="post-article article-<?php $category = get_the_category();echo $category[0]->slug;?>">
+						<a href="<?php the_permalink(); ?>">
+							<header>
+									<?php if( has_post_thumbnail()): ?>
+										<figure style="background:url(<?php the_post_thumbnail_url('full'); ?>)no-repeat center; background-size:cover">
+										</figure>
+									<?php else: ?>
+										<figure style="background:url(<?php bloginfo('template_url'); ?>/img/hero.png)no-repeat center; background-size:cover">
+										</figure>
+									<?php endif; ?>
+							</header>
+							<main>
+								<div class="post-subtitle-info">
+									<h4 class="subtitle"><?php $category = get_the_category();echo $category[0]->name;?></h4>
+									<p class="basic-font">
+										<?php echo do_shortcode(
+										'[rt_reading_time postfix="min read" postfix_singular="min read" label=""]'
+									); ?>
+									</p>
+								</div>
+						
+								<h3 class="title"><?php the_title(); ?></h3>
+							</main>
+						</a>
+						<footer class="post-info">
+							<div class="post-info-detail">
+									<div class="post-author">
+										<?php $author_id=$post->post_author; ?>
+										<?php
+											 echo get_avatar(get_the_author_meta('user_email'), $size = '48');
+										?>
+										<div>
+											<p><?php the_author_meta( 'display_name' , $author_id ); ?></p>
+											<p><?php the_time( get_option('date_format')); ?></p>
+										</div>
+								</div>
+									<div class="actions">
+									<?php echo do_shortcode(
+										'[wp_ulike]'
+									); ?>
+										<!-- <img src="<?php bloginfo('template_url'); ?>/img/like.svg"> -->
+										<img src="<?php bloginfo('template_url'); ?>/img/share.svg">
+									</div>
+								</div>
+						</footer>
 					</article>
-					<article class="top-article">
-						<figure>
-							<img src="img/hero.png" alt="">
-						</figure>
-						<h3 class="color-title-cat2"> Investigation</h3>
-						<h4 class="basic-font">
-							An Astronaut's View Of Mount Etna's Recent Volcanic Eruption
-						</h4>
-					</article>
-					<article class="top-article">
-						<figure>
-							<img src="img/hero.png" alt="">
-						</figure>
-						<h3 class="color-title-cat2"> Investigation</h3>
-						<h4 class="basic-font">
-							An Astronaut's View Of Mount Etna's Recent Volcanic Eruption
-						</h4>
-					</article>
-					<article class="top-article">
-						<figure>
-							<img src="img/hero.png" alt="">
-						</figure>
-						<h3 class="color-title-cat3"> Events</h3>
-						<h4 class="basic-font">
-							An Astronaut's View Of Mount Etna's Recent Volcanic Eruption
-						</h4>
-					</article>
-				</div> -->
-
+					<?php endwhile; ?><?php wp_reset_postdata(); ?>
+				</div>
 			</div>
 		</section>
+		
 		<section class="blog container">
 			<div class="row">
 				<main class="col-sm-12">
 				<?php query_posts('offset=1'); if( have_posts() ) : while( have_posts() ) : the_post(); ?>
 
-					<article class="article-<?php $category = get_the_category();echo $category[0]->slug;?>">
+					<article class="post-article article-<?php $category = get_the_category();echo $category[0]->slug;?>">
 						<a href="<?php the_permalink(); ?>">
 							<header>
 									<?php if( has_post_thumbnail()): ?>
